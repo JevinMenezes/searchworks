f8b06eef1d440293e75321797b821699cdde7b28
sweta2806 <58547737+sweta2806@users.noreply.github.com>
2020-05-31 23:59:46 +0530

    Changes in Android Manifest.xml

--- lib/main.dart
+++ lib/main.dart
@@ -13,12 +13,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Search Works!',
+      title: 'Hello Gaya!',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: WebViewExample(title: 'Searchworks'),
+      home: WebViewExample(title: 'HelloGaya'),
     );
   }
 }
